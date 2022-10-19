2362e6bcb9431711fc89bee494a4185a3e6532d7
Agustin <agustin.alonso@globallogic.com>
2022-10-19 15:31:50 -0300

    change cors

--- server.js
+++ server.js
@@ -16,10 +16,7 @@ const app = express();
 // app.use(cookieParser());
 // app.use(bodyParser.json({ extended: true }));
 app.use(express.json())
-app.use(cors({
-    credentials: true,
-    origin: 'http://localhost:3000'
-}))
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Ok')
