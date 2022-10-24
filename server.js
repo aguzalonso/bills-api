03ce3bc94b3fddc49d77f28b1ab6d5f9e6a7d819
Agustin <agustin.alonso@globallogic.com>
2022-10-24 16:50:09 -0300

    port

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ const app = express();
 app.use(express.json())
 app.use(cors())
 
+const PORT = process.env.PORT || 4000
+
 router.get('/', (req, res) => {
     try {
         res.json({
@@ -133,4 +135,4 @@ app.post('/deleteBills', async (req, res) => {
 })
 
 
-app.listen(4000)
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`))
